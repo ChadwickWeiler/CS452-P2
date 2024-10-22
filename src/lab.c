f5ab982943be092d6b48b71bea8db1b723b4f80a
ChadwickWeiler <77372775+ChadwickWeiler@users.noreply.github.com>
2024-10-22 02:21:38 +0000

    enqueue implemented

--- src/lab.c
+++ src/lab.c
@@ -3,32 +3,59 @@
 #include <stdlib.h>
 
 
-//Creating the structure of a simple linked list
+//Creating the structure of a simple doubly linked list
 typedef struct Node{
-    int object; 
-    struct Node* next;
+    int data; 
+    struct Node* oldest;
+    struct Node* newest;    
 } Node;
 
 typedef struct queue_t{
     int size;
+    int max_size;
     Node* head;
     Node* tail;
 } queue_t;
 
 
 queue_t queue_init(int capacity){
+    
     queue_t new_q;
-    new_q.size = capacity;
+    new_q.size = 0;
+    new_q.max_size = capacity;
     new_q.head = NULL;
     new_q.tail = NULL;
+
+    return new_q;
 }
 
 
 void queue_destroy(queue_t q);
 
 
-void enqueue(queue_t q, void *data);
+void enqueue(queue_t q, void *data){
 
+Node* n = (Node*)malloc(sizeof(Node));
+    if(q.size < q.max_size){
+
+            n->data = *(int*)data;
+
+            if(q.size != 0){
+            q.tail->newest = n;
+            q.tail = n; 
+            }
+
+            else{
+            q.head = n;
+            q.tail = n;
+            n->newest = NULL;
+            n->oldest = NULL;
+            }
+
+            q.size ++;
+        }
+    return q;
+}
 
 void *dequeue(queue_t q);
 
